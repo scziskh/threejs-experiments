0aaf0d2a333a2521a9ef05e9324af8da7a016f4e
scziskh <muzychukserhii@gmail.com>
2023-02-05 22:14:51 +0200

    continiue experiments

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,73 @@
 import * as THREE from "three";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const App = () => {
-  // new scene
+  // creating scene
   const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xe5e5e5);
 
-  // new camera (field of view, camera aspect ratio, camera frustum near plane, camera frustum far plane)
-  const camera = new THREE.PerspectiveCamera(90, 4 / 3, 0.1, 10000);
+  // creating camera
+  const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 256);
+  camera.position.set(10, 10, 10);
+  camera.lookAt(0, 0, 0);
 
-  // new WebGlRenderer
+  // creating renderer
   const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(512, 512);
 
-  // size of renderer
-  renderer.setSize(800, 600);
+  // creating Orbit Controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.update();
 
-  // set camera position
-  camera.position.set(0, 0, 5);
+  // add 3D object to scene
+  // const cubeGeometry = new THREE.BoxGeometry(128, 128, 128);
+  // const cubeMaterial = new THREE.MeshNormalMaterial();
+  // const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  // scene.add(cube);
 
-  // set camera look (point in center screen)
-  camera.lookAt(1, 1, 1);
+  // grid
 
-  // create and add mesh (cube) to scene
-  const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-  const cubeMaterial = new THREE.MeshBasicMaterial({
-    color: 0xff00ff,
-    transparent: true,
-    opacity: 0.1,
+  const gridGeometry = new THREE.BoxGeometry(1, 1, 1);
+  const gridMaterial = new THREE.LineDashedMaterial({
+    color: 0xffffff,
+    linewidth: 1,
+    scale: 1,
+    dashSize: 1,
+    gapSize: 1,
   });
-  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  scene.add(cube);
-
-  // load object GLB
-  const loader = new GLTFLoader();
-
-  loader.load(
-    "/model.glb",
-    (glbf) => {
-      scene.add(glbf.scene);
-      console.log({ glbf });
-    },
-    function (xhr) {
-      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-    },
-    (error) => {
-      console.error(error);
-    }
+  const grid = new THREE.InstancedMesh(
+    gridGeometry,
+    gridMaterial,
+    Math.pow(10, 3)
   );
 
-  // rotate scene
-  scene.rotateX(0.5);
-  scene.rotateZ(0.5);
+  let i = 0;
 
-  // background color
-  scene.background = new THREE.Color(0xe0e0e0);
+  const matrix = new THREE.Matrix4();
 
-  // render
-  renderer.render(scene, camera);
+  for (let x = 0; x < 10; x++) {
+    for (let y = 0; y < 10; y++) {
+      for (let z = 0; z < 10; z++) {
+        matrix.setPosition(5 - x, 5 - y, 5 - z);
 
-  // domElement of renderer
-  const { domElement } = renderer;
+        grid.setMatrixAt(i, matrix);
 
-  return <div>{domElement}</div>;
+        i++;
+      }
+    }
+  }
+
+  scene.add(grid);
+
+  // callback update dom and return it
+  const getThreeDom = () => {
+    requestAnimationFrame(getThreeDom);
+    renderer.render(scene, camera);
+
+    return renderer.domElement;
+  };
+
+  return <div>{getThreeDom()}</div>;
 };
 
 export default App;
