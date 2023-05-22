ed7cec2c90191200db94a24d6533f071dac7cf56
scziskh <muzychukserhii@gmail.com>
2023-05-22 23:50:32 +0300

    TrackboxControl

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,54 @@
+import { onMount } from "solid-js";
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { TrackballControls } from "three/examples/jsm/controls/TrackballControls.js";
 
 const App = () => {
+  onMount(() => {
+    ref.appendChild(renderer.domElement);
+
+    const controls = new TrackballControls(camera, renderer.domElement);
+    controls.rotateSpeed = 50;
+    controls.zoomSpeed = 1;
+    controls.panSpeed = 1;
+    const animate = () => {
+      requestAnimationFrame(animate);
+      controls.update();
+      render();
+    };
+    const render = () => {
+      renderer.render(scene, camera);
+    };
+
+    animate();
+  });
+
+  let ref;
+
   // creating scene
   const scene = new THREE.Scene();
   scene.background = new THREE.Color(0xe5e5e5);
 
   // creating camera
   const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 256);
-  camera.position.set(10, 10, 10);
+  camera.position.set(3, 3, 3);
   camera.lookAt(0, 0, 0);
 
   // creating renderer
   const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(512, 512);
+  renderer.setSize(1024, 1024);
 
-  // creating Orbit Controls
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.update();
-
-  // add 3D object to scene
-  // const cubeGeometry = new THREE.BoxGeometry(128, 128, 128);
-  // const cubeMaterial = new THREE.MeshNormalMaterial();
-  // const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  // scene.add(cube);
-
-  // grid
-
-  const gridGeometry = new THREE.BoxGeometry(1, 1, 1);
-  const gridMaterial = new THREE.LineDashedMaterial({
-    color: 0xffffff,
-    linewidth: 1,
-    scale: 1,
-    dashSize: 1,
-    gapSize: 1,
-  });
-  const grid = new THREE.InstancedMesh(
-    gridGeometry,
-    gridMaterial,
+  // creating mesh
+  const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+  boxGeometry.computeVertexNormals();
+  const boxMaterial = new THREE.MeshNormalMaterial();
+  const box = new THREE.InstancedMesh(
+    boxGeometry,
+    boxMaterial,
     Math.pow(10, 3)
   );
+  scene.add(box);
 
-  let i = 0;
-
-  const matrix = new THREE.Matrix4();
-
-  for (let x = 0; x < 10; x++) {
-    for (let y = 0; y < 10; y++) {
-      for (let z = 0; z < 10; z++) {
-        matrix.setPosition(5 - x, 5 - y, 5 - z);
-
-        grid.setMatrixAt(i, matrix);
-
-        i++;
-      }
-    }
-  }
-
-  scene.add(grid);
-
-  // callback update dom and return it
-  const getThreeDom = () => {
-    requestAnimationFrame(getThreeDom);
-    renderer.render(scene, camera);
-
-    return renderer.domElement;
-  };
-
-  return <div>{getThreeDom()}</div>;
+  return <div ref={ref}></div>;
 };
 
 export default App;
